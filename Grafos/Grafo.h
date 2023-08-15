bc136da97512e85e18a9de651bfe57f1937e04b0
Antonio <ahmbarreiros.cs@gmail.com>
2023-08-15 13:50:54 -0300

    buscas em grafos

--- Grafos/Grafo.h
+++ Grafos/Grafo.h
@@ -9,3 +9,12 @@ int insere_aresta(Grafo *gr, int orig, int dest, int eh_digrafo, float peso);
 int remove_aresta(Grafo *gr, int orig, int dest, int eh_digrafo);
 
 void libera_grafo(Grafo *gr);
+
+void buscaProfundidade(Grafo *gr, int ini, int* visitado, int cont);
+
+void buscaProfundidade_Grafo(Grafo *gr, int ini, int *visitado);
+
+void buscaLargura_Grafo(Grafo *gr, int ini, int *visitado);
+
+int procuraMenorDistancia(float *dist, int *visitado, int NV);
+void menorCaminho_Grafo(Grafo *gr, int ini, int *ant, float *dist);
--- Grafos/main.c
+++ Grafos/main.c
@@ -3,10 +3,28 @@
 
 int main() {
     Grafo *gr;
+    int eh_digrafo = 1;
     gr = cria_grafo(10, 7, 0);
-    insere_aresta(gr, 0, 1, 0, 0);
-    insere_aresta(gr, 1, 3, 0, 0);
-    remove_aresta(gr, 0, 1, 0);
+    insere_aresta(gr, 0, 1, eh_digrafo, 0);
+    insere_aresta(gr, 1, 3, eh_digrafo, 0);
+    insere_aresta(gr, 1, 2, eh_digrafo, 0);
+    insere_aresta(gr, 2, 4, eh_digrafo, 0);
+    insere_aresta(gr, 3, 0, eh_digrafo, 0);
+    insere_aresta(gr, 3, 4, eh_digrafo, 0);
+    insere_aresta(gr, 4, 1, eh_digrafo, 0);
+    int vis[5];
+    int ant[5];
+    float dist[5];
+
+    // Busca em Profundidade
+    buscaProfundidade_Grafo(gr, 0, vis);
+    
+    // Busca em Largura
+    buscaLargura_Grafo(gr, 0, vis);
+
+    // Algoritmo de Dijkstra
+    menorCaminho_Grafo(gr, 0, ant, dist);
+
     libera_grafo(gr);
     return 0;
 }
